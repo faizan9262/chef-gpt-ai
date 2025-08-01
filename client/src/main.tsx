3b359092b97a022286603900633914ff057ef4f1
faizan9262 <faizanshaikh9262@gmail.com>
2025-08-01 12:13:48 +0530

    Vercel server url added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,8 +7,8 @@ import { Toaster } from "sonner";
 import { DishContextProvider } from "./Context/DishContext.tsx";
 import { AuthProvider } from "./Context/AuthContext.tsx";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
-// axios.defaults.baseURL = "https://chef-gpt-server.vercel.app/api";
+// axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://chef-gpt-ai-server.vercel.app/api";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
