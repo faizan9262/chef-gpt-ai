c16c2772ca61f876394c584f4b3711d9c7f9b73f
faizan9262 <faizanshaikh9262@gmail.com>
2025-08-01 12:34:45 +0530

    Vercel.json added

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,7 @@
 import app from "../src/index";
 import { createServer } from "http";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const server = createServer(app);
   server.emit("request", req, res);
 }
