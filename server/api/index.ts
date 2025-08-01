bca4aec2f426945d6296e83d3086b23f7f45b2f1
faizan9262 <faizanshaikh9262@gmail.com>
2025-08-01 14:47:25 +0530

    vercel.json modifiled

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,2 +1,10 @@
+// api/index.ts
 import app from "../src/index";
-export default app;
+import { Server } from "http";
+import { NowRequest, NowResponse } from "@vercel/node";
+
+const server = new Server(app);
+
+export default function handler(req: NowRequest, res: NowResponse) {
+  server.emit("request", req, res);
+}
